e4e4fd6499493a2c70a70b07dcf515057c47bb1d
Alexey Tyutin <atyutin@dalet.com>
2022-03-02 22:58:40 +0300

    Event handlers

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -8,6 +8,20 @@ angular.
     templateUrl: 'phone-detail/phone-detail.template.html',
     controller: ['$http', '$stateParams',
       function PhoneDetailController() {
+        var ctrl = this;
+        this.mainImageUrl = '';
+
+        this.$onInit = function() {
+          this.setImage(this.phone.images[0]);
+        }
+
+        this.setImage = function(imageUrl) {
+          this.mainImageUrl = imageUrl;
+        };
+
+        this.onDblclick = function onDblclick(imageUrl) {
+          alert('You double-clicked image: ' + imageUrl);
+        };
       }
     ]
   });
--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -77,9 +77,21 @@ describe('PhoneCat Application', function() {
       expect(element(by.binding('$ctrl.phone.name')).getText()).toBe('Nexus S');
     });
 
-    it('should display 4 thumbnail images', function() {
-      var imageList = element.all(by.repeater('img in $ctrl.phone.images'));
-      expect(imageList.count()).toBe(4);
+    it('should display the first phone image as the main phone image', function() {
+      var mainImage = element(by.css('img.phone'));
+
+      expect(mainImage.getAttribute('src')).toContain('img/phones/nexus-s.0.jpg');
+    });
+
+    it('should swap the main image when clicking on a thumbnail image', function() {
+      var mainImage = element(by.css('img.phone'));
+      var thumbnails = element.all(by.css('.phone-thumbs img'));
+
+      thumbnails.get(2).click();
+      expect(mainImage.getAttribute('src')).toContain('img/phones/nexus-s.2.jpg');
+
+      thumbnails.get(0).click();
+      expect(mainImage.getAttribute('src')).toContain('img/phones/nexus-s.0.jpg');
     });
 
   });
